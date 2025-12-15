f86b2dda74bcee53e912ca14a7c94e09c24c5a40
Koralys Figueroa <koralys12@yahoo.com>
2025-12-14 21:22:29 -0500

    hola

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -43,4 +43,39 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 	if (i + 1 != high)
 	{
 		swap_ints(&array[i + 1], &array[high]);
-		print_array(a_
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursively sorts using Quick sort
+ * @array: Array of integers
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ * @array: Array of integers
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
