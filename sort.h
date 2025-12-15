53c0720fb5a2b50e233bfb866f344412fbc0695f
Koralys Figueroa <koralys12@yahoo.com>
2025-12-14 21:32:33 -0500

    hola

--- sort.h
+++ sort.h
@@ -1,26 +1,24 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* Struct for doubly linked list used in insertion sort */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* Prototypes for printing arrays and lists */
+/* Print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algorithms prototypes */
+/* Sorting algorithms */
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
