93f61d719111a74640340216d77c762b4633f442
Koralys Figueroa <koralys12@yahoo.com>
2025-12-14 20:24:29 -0500

    hola

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-hola soy koralys
+#sorting Algorithms
+
+thos project implements the bubble sort algorithm in c.
+## bubble sort 
+
+bubble sort repeatedly compares adjacent elements and swaps them if they e in the wrong order.
+
+time complexity 
+
+Best case: o(n)
+Average case: o(n^2)
+Worst case: o(n^2)
+
+
+file
+
+0-bubble_sort.c
+0-0
+sort.h
+print_array.c
--- sort.h
+++ sort.h
@@ -4,27 +4,17 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * print_array - Prints an array of integers
+ * @array: The array to print
+ * @size: Number of elements in the array
  */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
-
-/* Provided printing functions */
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 
-/* Sorting algorithms */
+/**
+ * bubble_sort - Sorts an array of integers using Bubble sort
+ * @array: The array to sort
+ * @size: Number of elements in the array
+ */
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
