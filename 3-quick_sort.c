b13611ef7a888ba5345eda77d8e17edcaf56cc1b
Koralys Figueroa <koralys12@yahoo.com>
2025-12-14 21:28:18 -0500

    hola

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -55,4 +55,18 @@ int lomuto_partition(int *array, int low, int high, size_t size)
             if (i != j)
             {
                 temp = array[i];
-                array[i]
+                array[i] = array[j];
+                array[j] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+    if (i + 1 != high)
+    {
+        temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        print_array(array, size);
+    }
+    return (i + 1);
+}
