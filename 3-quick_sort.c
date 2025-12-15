1c626b05edecdec978c1fd028571603610d843b2
Koralys Figueroa <koralys12@yahoo.com>
2025-12-14 21:26:17 -0500

    hola

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,33 +3,56 @@
 #include <stddef.h>
 
 /**
- * selection_sort - sorts an array of integers in ascending order
- *                  using the Selection sort algorithm
+ * quick_sort - sorts an array of integers in ascending order
+ *              using the Quick sort algorithm (Lomuto partition)
  * @array: pointer to the array
  * @size: size of the array
  */
-void selection_sort(int *array, size_t size)
-{
-    size_t i, j, min_idx;
-    int temp;
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
+/**
+ * quick_sort - entry point for Quick sort
+ */
+void quick_sort(int *array, size_t size)
+{
     if (!array || size < 2)
         return;
 
-    for (i = 0; i < size - 1; i++)
+    quick_sort_recursive(array, 0, (int)size - 1, size);
+}
+
+/**
+ * quick_sort_recursive - recursive function for Quick sort
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+    int pivot_idx;
+
+    if (low < high)
     {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            print_array(array, size);
-        }
+        pivot_idx = lomuto_partition(array, low, high, size);
+        quick_sort_recursive(array, low, pivot_idx - 1, size);
+        quick_sort_recursive(array, pivot_idx + 1, high, size);
     }
 }
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+    int j, temp;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                temp = array[i];
+                array[i]
